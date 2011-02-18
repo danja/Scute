4bd971523ef636f9024957f5a167dad56d5d6508
Danny Ayers <danny.ayers@gmail.com>
2011-02-18 13:26:28 +0100

    placeholder

---
+++ src/org/hyperdata/scute/graphmanager/GraphManagerPanel.java
@@ -0,0 +1,21 @@
+
+package org.hyperdata.scute.graphmanager;
+
+import javax.swing.JPanel;
+
+/**
+ * @author danny
+ *
+ */
+public class GraphManagerPanel extends JPanel {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
