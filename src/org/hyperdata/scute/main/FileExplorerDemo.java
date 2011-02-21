7677b6ee1204938f8b69214020e25664618e77cb
Danny Ayers <danny.ayers@gmail.com>
2011-02-21 11:33:53 +0100

    a standalone runner for FileExplorer

---
+++ src/org/hyperdata/scute/main/FileExplorerDemo.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.hyperdata.scute.main;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+import org.hyperdata.scute.filemanager.FileExplorerPanel;
+
+/**
+ * @author danny
+ *
+ */
+public class FileExplorerDemo {
+
+	   public static void main( String[] argv ) {
+	        JFrame frame = new JFrame( "File Explorer" );
+
+	        frame.addWindowListener( new WindowAdapter() {
+	                                     @Override
+										public void windowClosing( WindowEvent e ) {
+	                                         System.exit( 0 );
+	                                     }
+	                                 });
+
+	       
+
+	        frame.getContentPane().add( new FileExplorerPanel() );
+
+	        frame.setSize( 400, 400 );
+	        frame.pack();
+	        frame.setVisible(true);
+	    }
+}
